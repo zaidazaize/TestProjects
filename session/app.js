809b7991be02d2c25136628ded6caca72896d29e
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-02-28 01:55:32 +0530

    login session imp

--- session/app.js
+++ session/app.js
@@ -5,7 +5,8 @@ const { urlencoded } = require('express');
 const parseUrl = require('parseurl')
 const MongoStore = require('connect-mongo');
 const parseurl = require('parseurl');
-
+const escapeHTML = require('escape-html');
+var data = []
 const app = express();
 const port = process.env.port || 3000;
 
@@ -42,6 +43,37 @@ app.use(session({
 //     req.session.views[pathname] = (req.session.views[pathname] || 0) +1
 //     next()
 // })
+
+function authenticate(req, res, next) {
+    if (req.session.user) next()
+    else next('route')
+}
+app.get('/', authenticate, (req, res) => {
+    res.send("Hi, " + escapeHTML(req.session.user)+ "!" +'<a href="/logout">Logout</a>')
+})
+app.get('/', (req, res) => {
+res.send('<form action="/login" method="post">' +
+    'Username: <input name="user"><br>' +
+    'Password: <input name="pass" type="password"><br>' +
+    '<input type="submit" text="Login"></form>')
+})
+
+app.post('/login', (req, res,next) => {
+    const d = {user : req.body.user,password : req.body.password}
+    data[data.length] = d;
+    req.session.regenerate(function (err) {
+        if(err) {
+            next(err)
+        }
+        req.session.user = d.user
+        req.session.save(function (err) {
+            if (err) return err;
+            res.redirect('/')
+        })
+        
+
+   }) 
+})
 
 app.get('/one', (req, res) => {
     // console.log(req)
