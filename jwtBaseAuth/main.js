a2cb8aef2e431f748d58c5fa35cc44b20f5c3c1b
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-03-01 19:55:56 +0530

    chaneges

---
+++ jwtBaseAuth/main.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app =  express()
+
+app.get('/', (req, res) => {
+    
+})
+
+app.listen(3000, (req, res) => {
+    console.log("server is running on port 3000")
+})
